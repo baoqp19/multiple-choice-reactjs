d3711aa15da1c4eb6087e8aa57d03422e1433bbc
= <minhducdtr70@gmail.com>
2025-09-01 13:29:29 +0700

    partnet logo

---
+++ landingPageTaiwindCss/js/main.js
@@ -0,0 +1,18 @@
+import { partnerLogos, productList, partnerLogoBasePath } from "./data.js";
+
+/** ========
+ *
+ *  Parner Logo
+ */
+$(function () {
+  const container = document.getElementById("partner-logo-list");
+
+  partnerLogos.forEach((logo) => {
+    const img = document.createElement("img");
+    img.src = partnerLogoBasePath + logo.fileName;
+    img.alt = logo.alt;
+    img.classList.add("logo-ticker-image");
+
+    container.appendChild(img);
+  });
+});
